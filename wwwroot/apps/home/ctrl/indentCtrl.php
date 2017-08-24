b147ff9927353b0314d270f77f5e1321a36feb6c
zhaojiangetcunjicom <zhaojian@getcunji.com>
2017-08-24 21:52:05 +0800

    1

--- wwwroot/apps/home/ctrl/indentCtrl.php
+++ wwwroot/apps/home/ctrl/indentCtrl.php
@@ -220,8 +220,10 @@ class indentCtrl extends baseCtrl{
       // 优惠券（订单满多少立即多少？）
       $iprice = isset($_GET['iprice']) ? intval($_GET['iprice']) : 0;
       $price = isset($_GET['price']) ? intval($_GET['price']) : 0;
-      if ($data['total_money'] > $iprice) {
-        $data['total_money'] = bcsub($data['total_money'], $price, 0);
+      if ($iprice !=0 && $price !=0) {
+        if ($data['total_money'] > $iprice) {
+          $data['total_money'] = bcsub($data['total_money'], $price, 0);
+        }
       }
       $data['total_money'] = bcmul($data['total_money'], 100, 0);
       // 统一下单
