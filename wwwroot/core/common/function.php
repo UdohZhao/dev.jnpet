854040d46feee25d79a2692b070d99b63bcb4b1b
zhaojiangetcunjicom <zhaojian@getcunji.com>
2017-08-24 12:00:17 +0800

    zhaojian

--- wwwroot/core/common/function.php
+++ wwwroot/core/common/function.php
@@ -273,3 +273,20 @@ function makeSign($data){
   $result=strtoupper($sign);
   return $result;
 }
+
+/**
+ * xml转换成数组
+ */
+function xmlToArray($xml){
+
+ //禁止引用外部xml实体
+
+libxml_disable_entity_loader(true);
+
+$xmlstring = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
+
+$val = json_decode(json_encode($xmlstring),true);
+
+return $val;
+
+}
