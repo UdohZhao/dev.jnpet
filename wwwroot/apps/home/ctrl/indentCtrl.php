2157089bdff50c1544eb05a6fc11f5880fb95b2e
zhaojiangetcunjicom <zhaojian@getcunji.com>
2017-10-12 16:39:37 +0800

    zhaojian

--- wwwroot/apps/home/ctrl/indentCtrl.php
+++ wwwroot/apps/home/ctrl/indentCtrl.php
@@ -319,8 +319,6 @@ class indentCtrl extends baseCtrl{
         }
       }
       $data['total_money'] = bcmul($data['total_money'], 100, 0);
-      echo J($this->openid);
-      die;
       // 统一下单
       $jsApiParameters = wxJsapiPay($this->openid,'宠物饲料',$data['inumber'],$data['total_money'],$this->id);
       echo J($jsApiParameters);
